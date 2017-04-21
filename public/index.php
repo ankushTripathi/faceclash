b1a931e6efcfb40f49a5cf367475cc7a47808b40
ankush tripathi <ankushtripathi0996@yahoo.com>
2017-04-22 01:16:31 +0530

    home route and img data

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
-
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
 
 /*
@@ -15,7 +14,7 @@
 | Composer provides a convenient, automatically generated class loader for
 | our application. We just need to utilize it! We'll simply require it
 | into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
+| loading any of our classes later on. It feels nice to relax.
 |
 */
 
@@ -40,8 +39,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | Run The Application
 |--------------------------------------------------------------------------
 |
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
+| Once we have the application, we can simply call the run method,
+| which will execute the request and send the response back to
 | the client's browser allowing them to enjoy the creative
 | and wonderful application we have prepared for them.
 |
@@ -50,7 +49,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+	$request = Illuminate\Http\Request::capture()
 );
 
 $response->send();
