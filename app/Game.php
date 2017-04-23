de20d6becd45635ea62e4f8a7855396e30c42e7c
ankush tripathi <ankushtripathi0996@yahoo.com>
2017-04-23 12:52:01 +0530

    add ranking algorithm

--- app/Game.php
+++ app/Game.php
@@ -14,19 +14,19 @@ class Game extends Model
         'loser'
     ];
 
-    public static function expectedWinner(){
+    //ELO - algorithm
 
+    public static function expected($rA,$rB){
+        return 1/(1+pow(10,($rB-$rA)/600));
     }
 
-    public static function win(){
-
+    public static function updateScore($score,$k = 24,$actual,$expected){
+        return ROUND($score + $k*($actual - $expected));
     }
-
-    public static function loss(){
-
-    }
-
-    public static function rank(){
-
+    public static function rank($score,$losses,$wins){
+        if($wins!==0)
+            return ROUND($score/(1+($losses/$wins)));
+        else
+            return ROUND($score/(1+($losses/0.1)));
     }
 }
